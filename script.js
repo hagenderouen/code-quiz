d727668f858ea4514c5d345f46f880fb4dac52d8
Hagen DeRouen <hagend87@gmail.com>
2021-02-17 13:47:21 -0600

    Add jQuery and function to display the question.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const quiz = {
     ],
     timeLeft: 75,
     isQuizInProgress: false,
+    currentQuestion: 0,
     calculateScore() {
         // do something
     },
@@ -19,8 +20,30 @@ const quiz = {
         // do something
     }
 }
+
+const mainEl = $('#main-content');
+
 // A form submit handler that navigates through the questions.
+
 // Display handler for quiz questions and choices.
+const displayQuestion = function(quiz) {
+    let questionIndex = quiz.currentQuestion;
+    let question = quiz.questions[questionIndex];
+    console.log(question);
+    // append question to main content
+    mainEl.append(`<h2>${question.question}</h2>`);
+    // creat the list for answer choices
+    let listEl = $('<ol>');
+    // loop through choices and append items to the list
+    for (var i = 0; i < question.choices.length; i++) {
+        listEl.append(`<li>${question.choices[i]}</li>`);
+    }
+    // append list to mainEl
+    mainEl.append(listEl);
+}
+
+displayQuestion(quiz);
+
 // Display handler for quiz scores.
 // Start quiz button and event listener to start the timer.
 // Detect incorrect answers and subtract from timer.
